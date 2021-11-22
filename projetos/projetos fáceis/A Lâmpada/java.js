37fbcdea949c6ef867eb100a173a9e687882adce
Carlos magno <83795938+Magno-programmer@users.noreply.github.com>
2021-11-22 10:26:09 -0300

    confirmar em 'main'

--- projetos/projetos fáceis/A Lâmpada/java.js
+++ projetos/projetos fáceis/A Lâmpada/java.js
@@ -3,26 +3,22 @@ const turnOnOff = document.getElementById('ligarDesligar');
 const body = document.getElementById('body');
 
 function on(){
-    if(!lampquebrada()){
-        lampada.src = './imagens/ligada.jpeg'
-        body.style.backgroundColor = 'white';
-        lampada.style.opacity = '1';
-        lampada.style.width ='158px';
-        lampada.style.height ='215px';
-        turnOnOff.textContent = 'Desligar';
-    }
+    lampada.src = './imagens/ligada.jpeg'
+    body.style.backgroundColor = 'white';
+    lampada.style.opacity = '1';
+    lampada.style.width ='158px';
+    lampada.style.height ='215px';
+    turnOnOff.textContent = 'Desligar';
     turnOnOff.style.backgroundColor = 'rgb(255, 0, 0)';
 }
 
 function off(){
-    if(!lampquebrada()){
-        lampada.src = './imagens/desligada.jpg'
-        body.style.backgroundColor = 'black';
-        lampada.style.opacity = '0.0274';
-        lampada.style.width ='158px';
-        lampada.style.height ='215px';
-        turnOnOff.textContent = 'Ligar';
-    }
+    lampada.src = './imagens/desligada.jpg'
+    body.style.backgroundColor = 'black';
+    lampada.style.opacity = '0.0274';
+    lampada.style.width ='158px';
+    lampada.style.height ='215px';
+    turnOnOff.textContent = 'Ligar';
     turnOnOff.style.backgroundColor = 'rgb(30, 124, 0)';
     
 }
@@ -36,20 +32,6 @@ function onOff(){
 }
 
 
-function lampquebrada(){
-    return lampada.src.indexOf('quebrada') > -1;
-}
-
-function quebra(){
-    lampada.src = './imagens/quebrada.jpg'
-    body.style.backgroundColor = 'black';
-    lampada.style.opacity = '0.11';
-    lampada.style.width ='158px';
-    lampada.style.height ='215px';
-    turnOnOff.textContent = 'Ligar';
-}
-
 turnOnOff.addEventListener('click', onOff);
 lampada.addEventListener('mouseenter', on);
 lampada.addEventListener('mouseleave', off);
-lampada.addEventListener('dblclick', quebra);
